f6c7714e04f3179bafa5d2903e245c29ebf61a0b
claudio rodriguez ore <claudio@debian>
2016-08-03 17:06:57 -0500

    corrigiendo un bug de pm

--- appmodules/autentificacion/vista/url.php
+++ appmodules/autentificacion/vista/url.php
@@ -1,5 +1,5 @@
 <?php
-$lugar = 1;
+$lugar = 4;
 if ( $lugar == 1 )     $base_url = 'http://192.168.1.162/neointelperu_apps/appmodules/';
 elseif ( $lugar == 2 ) $base_url = 'http://192.168.1.3/neointelperu_apps/appmodules/';
 elseif ( $lugar == 3 ) $base_url = 'http://localhost/neointelperu_apps/appmodules/';
